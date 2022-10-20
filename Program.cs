e88c4519ed34931d48a892acdb10b787c13dbdca
mark <markram0100@gmail.com>
2022-10-20 11:17:14 -0700

    done

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿//  mark ramirez
-//  10/18/2022
+//  10/20/2022
 //  Questions project
-// it askes your name and what time you woke up
+// it askes your name and what time you woke up and then tell you your name and what time you woke up
 
 int idk = 0;
 bool validation = true;
